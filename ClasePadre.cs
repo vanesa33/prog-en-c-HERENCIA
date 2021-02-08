61357b94020ae9ed9642b434a03f9c8c88aeae24
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:27:55 -0300

    herencia herencia

---
+++ ClasePadre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace herencia
+{
+    class ClasePadre
+    {
+
+        public void saludar()
+        {
+            Console.WriteLine("Hola");
+        }
+
+        public void despedirse()
+        {
+            Console.WriteLine("Adios");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClaseHija Valida = new ClaseHija();
+
+            Valida.saludar();
+            Valida.despedirse();
+            Console.ReadKey();
+        }
+    }
+}
